665c30ec51ad1b232d859a57dabf894a7cbb3590
Roberto Boscolo Figueredo <33357673+betosan83@users.noreply.github.com>
2020-10-21 15:48:54 -0300

    Initial project commit.

---
+++ src/main/java/rbf/springframework/joke/JokeappApplication.java
@@ -0,0 +1,13 @@
+package rbf.springframework.joke;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JokeappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JokeappApplication.class, args);
+    }
+
+}
---
+++ src/test/java/rbf/springframework/joke/JokeappApplicationTests.java
@@ -0,0 +1,13 @@
+package rbf.springframework.joke;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JokeappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
